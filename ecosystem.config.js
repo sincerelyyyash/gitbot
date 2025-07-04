1b1dc0f74b05a49978960d9b31386259b0d3815e
sincerelyyyash <yta0088@gmail.com>
2025-07-05 04:12:46 +0530

    big fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
   apps: [
     {
       name: "gitbot",
-      script: "../venv/bin/uvicorn",
+      script: "/root/venv/bin/uvicorn",
       args: "app.main:app --host 0.0.0.0 --port 8050",
-      interpreter: "../venv/bin/python",
+      interpreter: "/root/venv/bin/python",
       watch: false,
       env: {
         PYTHONUNBUFFERED: "1"
